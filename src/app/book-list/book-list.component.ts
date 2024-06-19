8c72a0bb2fcc303a06d0f54d2e2506c79aaf9374
IsPe123 <Peterislas@outlook.com>
2024-06-19 13:08:07 -0300

    Clase 3 finalizada

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './book';
 
 @Component({
   selector: 'app-book-list',
@@ -6,11 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './book-list.component.scss'
 })
 export class BookListComponent {
-  book = {
-    "libro": "La Biblia",
-    "autor": "Varios",
-    "paginas": 500,
-    "editorial": "Oceano",
-    "precio": 20000
-  }
+  books: Book[] = [
+    {
+      libro: 'La Biblia',
+      autor: 'Varios',
+      paginas: 500,
+      editorial: 'Oceano',
+      precio: 20000,
+      stock: 2,
+      id: 1,
+    },
+    {
+      libro: 'El Principito',
+      autor: 'Alguien',
+      paginas: 300,
+      editorial: 'Oceano',
+      precio: 10000,
+      stock: 0,
+      id: 2,
+    },
+    {
+      libro: 'Maestria',
+      autor: 'Robert Green',
+      paginas: 420,
+      editorial: 'Oceano',
+      precio: 23000,
+      stock: 2,
+      id: 3,
+    }
+  ];
+
 }
---
+++ src/app/book-list/book.ts
@@ -0,0 +1,9 @@
+export interface Book {
+    libro: string;
+    autor: string;
+    paginas: number;
+    editorial: string;
+    precio: number;
+    stock: number;
+    id: number;
+}
